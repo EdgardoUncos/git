d2854e1fe3833fc228f3b5d97855d6ec923bdc3e
euncos <euncos@gmail.com>
2023-09-13 15:22:46 -0300

    inicio de proyecto

---
+++ 01-clase/index.js
@@ -0,0 +1,10 @@
+
+console.log('Hello world')
+
+console.log('Hola Edgardo')
+
+let nombre = 'Berni';
+let provincia = 'Mendoza';
+let pais = 'Argentina'
+
+console.log('Buenas, mi nombre es ' + nombre + 'y vivo en la provincia de ' + provincia + ' que se encuentra en ' + pais)
